784cb7708709733ae0da8fbe8d61e3780af55f25
Jun Oh <kyungjunjoh@gmail.com>
2019-01-21 12:54:33 -0800

    Initial Commit

---
+++ ScrollViewImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleries
+//
+//  Created by Jun Oh on 2019-01-21.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Jun Oh on 2019-01-21.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
